c8603a24929ac26c242bfe12f1e15d0db95442cc
Pedro Gomes <pedro-gomes@ibm.com>
2023-05-23 09:34:25 -0300

    feat: create crud to server with prisma tables user and memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoute } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true,
+})
+
+app.register(memoriesRoute)
 app
   .listen({
     port: 3333,
