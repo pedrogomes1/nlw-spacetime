8b5db613dd717e605b79441eb3a435a9132056a7
Pedro Gomes <pedro-gomes@ibm.com>
2023-05-25 11:25:03 -0300

    chore: create new upload route server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoute } from './routes/memories'
 import { authRotes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,6 +27,7 @@ app.register(jwt, {
 
 app.register(authRotes)
 app.register(memoriesRoute)
+app.register(uploadRoutes)
 app
   .listen({
     port: 3333,
