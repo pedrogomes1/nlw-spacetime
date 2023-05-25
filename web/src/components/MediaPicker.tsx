66684d58969297c2be21a2132167a65752b271aa
Pedro Gomes <pedro-gomes@ibm.com>
2023-05-25 16:40:44 -0300

    chore: added submit form to create a new memory in web app

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+
+export default function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+    if (!files) return
+
+    const previewURL = URL.createObjectURL(files[0])
+    setPreview(previewURL)
+  }
+
+  return (
+    <>
+      <input
+        onChange={onFileSelected}
+        type="file"
+        name="coverUrl"
+        id="media"
+        accept="image/*"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        <img
+          src={preview}
+          alt="Preview image"
+          className="aspect-video w-full w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
